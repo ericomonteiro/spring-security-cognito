2c0e46435ae8660dc5db998abc47d4fd1193415b
Érico Monteiro <erico.cintra@gmail.com>
2021-10-24 15:40:44 -0300

    ^

---
+++ src/main/kotlin/ericomonteiro/com/github/springsecuritycognito/SpringSecurityCognitoApplication.kt
@@ -0,0 +1,11 @@
+package ericomonteiro.com.github.springsecuritycognito
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityCognitoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSecurityCognitoApplication>(*args)
+}
---
+++ src/test/kotlin/ericomonteiro/com/github/springsecuritycognito/SpringSecurityCognitoApplicationTests.kt
@@ -0,0 +1,13 @@
+package ericomonteiro.com.github.springsecuritycognito
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityCognitoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
