692fd4c2b1abffaf624c79b73acda751258b6262
ericomonteiro <HKKEpVOVkK060991>
2021-11-09 21:27:41 -0300

    set aws region as property variable

--- src/main/kotlin/ericomonteiro/com/github/springsecuritycognito/config/aws/AWSClients.kt
+++ src/main/kotlin/ericomonteiro/com/github/springsecuritycognito/config/aws/AWSClients.kt
@@ -3,32 +3,25 @@ package ericomonteiro.com.github.springsecuritycognito.config.aws
 
 import com.amazonaws.auth.AWSCredentials
 import com.amazonaws.auth.AWSStaticCredentialsProvider
-import com.amazonaws.regions.Regions
-import com.amazonaws.services.cognitoidentity.model.CognitoIdentityProvider
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClient
-import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class AWSClients (
-    private val awsCredentials: AWSCredentials
-) {
+    private val awsCredentials: AWSCredentials,
 
-    @Bean
-    fun s3Client(): AmazonS3 = AmazonS3ClientBuilder
-        .standard()
-        .withRegion(Regions.US_EAST_1)
-        .withCredentials(AWSStaticCredentialsProvider(awsCredentials))
-        .build()
+    @Value("\${aws.region}")
+    private val awsRegion: String
+) {
 
     @Bean
     fun cognitoClient(): AWSCognitoIdentityProvider =
         AWSCognitoIdentityProviderClient
             .builder()
-            .withRegion(Regions.US_EAST_2)
+            .withRegion(awsRegion)
             .withCredentials(AWSStaticCredentialsProvider(awsCredentials))
             .build()
 
