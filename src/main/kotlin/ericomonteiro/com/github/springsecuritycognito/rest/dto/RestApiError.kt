160175643eded68b93528d706de0ec7b5bf98ffd
Érico Monteiro <erico.cintra@gmail.com>
2022-01-11 21:21:46 -0300

    Exception handler for NotAuthorizedException

---
+++ src/main/kotlin/ericomonteiro/com/github/springsecuritycognito/rest/dto/RestApiError.kt
@@ -0,0 +1,6 @@
+package ericomonteiro.com.github.springsecuritycognito.rest.dto
+
+data class RestApiError(
+    val title: String,
+    val details: String
+)
--- src/main/kotlin/ericomonteiro/com/github/springsecuritycognito/rest/v1/SecurityController.kt
+++ src/main/kotlin/ericomonteiro/com/github/springsecuritycognito/rest/v1/SecurityController.kt
@@ -1,16 +1,26 @@
 package ericomonteiro.com.github.springsecuritycognito.rest.v1
 
+import com.amazonaws.services.cognitoidp.model.NotAuthorizedException
 import com.amazonaws.services.cognitoidp.model.SignUpResult
 import ericomonteiro.com.github.springsecuritycognito.model.User
 import ericomonteiro.com.github.springsecuritycognito.rest.dto.LoginRequestDto
 import ericomonteiro.com.github.springsecuritycognito.rest.dto.LoginResponseDto
+import ericomonteiro.com.github.springsecuritycognito.rest.dto.RestApiError
 import ericomonteiro.com.github.springsecuritycognito.rest.dto.SignUpDto
 import ericomonteiro.com.github.springsecuritycognito.service.SecurityService
+import java.net.URI
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
-import org.springframework.web.bind.annotation.*
-import java.net.URI
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
@@ -18,6 +28,7 @@ import java.net.URI
 class SecurityController(
     private val securityService: SecurityService
 ) {
+    private val logger: Logger = LoggerFactory.getLogger(SecurityController::class.java)
 
     @PostMapping("/login")
     fun authenticate(@RequestBody loginRequestDto: LoginRequestDto): ResponseEntity<LoginResponseDto> {
@@ -39,5 +50,9 @@ class SecurityController(
     fun user(): ResponseEntity<User> {
         return ResponseEntity.ok(securityService.userAuthenticated(SecurityContextHolder.getContext().authentication as JwtAuthenticationToken))
     }
+
+    @ExceptionHandler(NotAuthorizedException::class)
+    fun notAuthorizedExceptionExceptionHandler(ex: NotAuthorizedException): ResponseEntity<RestApiError> =
+        ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(RestApiError(title = ex.errorCode, details = ex.errorMessage))
 
 }
